a25f25f922c8a16fe03bef2db04c4a2ececbeb7c
EmilianoArias <32575456+Sazakuni7@users.noreply.github.com>
2024-11-08 11:22:59 -0300

    Update Adaptacion.cpp

--- Actividad 5 - Adaptacion/Adaptacion.cpp
+++ Actividad 5 - Adaptacion/Adaptacion.cpp
@@ -3,17 +3,16 @@
 #include <iostream>
 using namespace sf;
 int main() {
-    RenderWindow window(VideoMode(800, 600), "Actividad 5 - Adaptación", Style::Default);
-    window.setVerticalSyncEnabled(true);
+    RenderWindow window(VideoMode(800, 600), "Actividad 5 - AdaptaciÃ³n", Style::Default);
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
             if (event.type == Event::Resized) {
-                //Obtener el nuevo tamaño al redimensionar
+                //Obtener el nuevo tamaÃ±o al redimensionar
                 Vector2u newSize(event.size.width, event.size.height);
-                //Setear el tamaño a estos limites si se quiere redimensionar por debajo o encima de los mismos
+                //Setear el tamaÃ±o a estos limites si se quiere redimensionar por debajo o encima de los mismos
                 if (newSize.x <= 100) newSize.x = 100;
                 if (newSize.y <= 100) newSize.y = 100;
                 if (newSize.x >= 1000) newSize.x = 1000;
